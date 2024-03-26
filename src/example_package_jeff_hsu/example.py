8ed996dcb1ec5cf20095431adc1b4e768e6fbf7a
jeff.hsu <jeff.hsu@nextfortune.tw>
2024-03-27 03:51:29 +0800

    validators

--- src/example_package_jeff_hsu/example.py
+++ src/example_package_jeff_hsu/example.py
@@ -1,2 +1,11 @@
-def add_one(number):
-    return number + 3
+import validators
+
+def check_email(email: str):
+    if validators.email(email):
+        return(f"{email} is a email")
+    return(f"{email} is not a email")
+
+def check_url(url: str):
+    if validators.url(url):
+        return(f"{url} is a url")
+    return(f"{url} is not a url")
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,11 @@
-from example_package_jeff_hsu.example import add_one
+from example_package_jeff_hsu.example import check_email, check_url
 
-def test_add_one():
-    assert add_one(1) == 4
-    assert add_one(-1) == 2
-    assert add_one(0) == 3
+def test_check_email():
+    assert check_email("test@example.com") == "test@example.com is a email"
+    assert check_email("test_example.com") == "test_example.com is not a email"
+    assert check_email("test@example") == "test@example is not a email"
+
+def test_check_url():
+    assert check_url("https://www.example.com") == "https://www.example.com is a url"
+    assert check_url("www.example.com") == "www.example.com is not a url"
+    assert check_url("https://www.example") == "https://www.example is a url"
